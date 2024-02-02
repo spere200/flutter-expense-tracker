71d959714c78436e2d00ba43cb1dc0969f47d81c
Samuel Perez <sperez207@gmail.com>
2024-02-02 06:40:57 -0500

    The modal now properly displays text fields and buttons. The cancel button can be clicked to close the modal via Navigator.pop(context)

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,17 +10,54 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  // TextEditingControllers need to be diposed of when their container is disposed of,
+  // otherwise it stays in memory and negatively affects performance
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               label: Text("Title"),
             ),
+          ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: "\$",
+              label: Text("Amount"),
+            ),
+          ),
+          Row(
+            children: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text("Cancel")),
+              ElevatedButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                    print(_amountController.text);
+                  },
+                  child: const Text("Save Expense")),
+              const SizedBox(width: 10),
+            ],
           ),
         ],
       ),
