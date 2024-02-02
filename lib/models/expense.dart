17edd309c77a7b02d8fd032fa76b111bd3d47d3a
Samuel Perez <sperez207@gmail.com>
2024-02-02 02:13:13 -0500

    Added the intl package for date formatting The expense model now has a category.icon map and uses intl to return formatted dates using a getter method.

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
+final formatter = DateFormat.yMd();
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
   final String id;
@@ -12,4 +22,8 @@ class Expense {
   final Category category;
 
   Expense(this.title, this.amount, this.date, this.category) : id = uuid.v4();
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
